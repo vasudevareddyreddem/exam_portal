bcceb504660e617aa73308e2326c378160b389f8
bayapucg <bayapu.edu004@gmail.com>
2018-10-08 11:36:37 +0530

    addign
    timmer

--- choose-option.php
+++ choose-option.php
@@ -5,9 +5,12 @@
       <div class="row">
         <div class="col-md-9">
 		<div class="box-data">
-            <div class="box-header">
-              <h3 class="box-title">Start Exam</h3>
+              <div class="box-header">
+              <h3 class="box-title pull-left">Start Exam</h3>
+              <h3 class="btn btn-warning btn-lg pull-right" id="demo"></h3>
             </div>
+			<div class="clearfix"></div>
+		
 			<hr>
             <!-- /.box-header -->
             <div class="box-body table-responsive "  style="padding:20px;">
@@ -80,6 +83,34 @@
     </section>
 		</div>
 	</div>
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
 
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+</script>
 <?php include("footer.php"); ?>
    
